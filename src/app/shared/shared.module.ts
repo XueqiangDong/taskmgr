a0797f5f70f560ea9996ea6c511e39b5f0ebff7e
Xueqiang Dong <dxq7541@gmail.com>
2018-04-29 00:35:40 -0400

    注册页面

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,18 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {
   MatIconModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatCardModule, MatInputModule,
-  MatListModule, MatSlideToggleModule
+  MatListModule, MatSlideToggleModule, MatGridListModule
 } from '@angular/material';
 import {LoginModule} from '../login/login.module';
 
 @NgModule({
   imports: [
     CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatCardModule,
-    MatInputModule, MatListModule, MatSlideToggleModule,
+    MatInputModule, MatListModule, MatSlideToggleModule, MatGridListModule,
   ],
   exports: [
     CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatCardModule,
-    MatInputModule, MatListModule, MatSlideToggleModule,
+    MatInputModule, MatListModule, MatSlideToggleModule, MatGridListModule,
   ],
   declarations: []
 })
